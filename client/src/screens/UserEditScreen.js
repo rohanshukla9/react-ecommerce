9846205806a84e6fc6d8ee692d67221ecdfea385
unknown <voidrohan@gmail.com>
2020-11-21 02:01:24 +0530

    User edit and update commit

---
+++ client/src/screens/UserEditScreen.js
@@ -0,0 +1,126 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/userConstants'
+
+
+const UserEditScreen = ({match, history}) => {
+
+  const userId = match.params.id
+  const [name, setName] = useState('')
+  const [phone_number, setPhoneNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [isAdmin, setIsAdmin] = useState(false)
+  const [message, setMessage] = useState(null)
+
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector(state => state.userDetails)
+
+  const { loading, error, user } = userDetails
+
+  const userUpdate = useSelector(state => state.userUpdate)
+
+  const { loading:loadingUpdate, error:errorUpdate, success:successUpdate } = userUpdate
+
+
+  useEffect(() => {
+    if(successUpdate) {
+      dispatch({type: USER_UPDATE_RESET})
+      history.push('/admin/userlist')
+    } else {
+      if(!user.name || user._id !== userId) {
+        dispatch(getUserDetails(userId))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+        setPhoneNumber(user.phone_number)
+        setIsAdmin(user.isAdmin)
+      }
+    }
+
+  
+  }, [dispatch, history, userId, user, successUpdate])
+
+  const submitHandler = (e) => {
+
+
+    e.preventDefault()
+
+    dispatch(updateUser({ _id: userId, name, email, phone_number, isAdmin}))
+
+
+   
+
+   //dispatch register
+  }
+  return (
+    <>
+    <Link to='/admin/userlist' className='btn btn-light my-3'>
+      Go Back
+    </Link>
+    <FormContainer>
+      <h1>Edit User Details</h1>
+      {loadingUpdate && <Loader />}
+      {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
+      {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+    
+        <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name'>
+          <Form.Label>Full Name:</Form.Label>
+          <Form.Control type='name' 
+          placeholder='Enter Name' 
+          value={name}
+          onChange={(e) => setName(e.target.value)}>
+
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId='phone_number'>
+          <Form.Label>Phone Number:</Form.Label>
+          <Form.Control type='phone_number' 
+          placeholder='Enter Phone No.' 
+          value={phone_number}
+          onChange={(e) => setPhoneNumber(e.target.value)}>
+
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type='email' placeholder='Enter Email' value={email}
+          onChange={(e) => setEmail(e.target.value)}>
+
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='isadmin'>
+          <Form.Label>Is Admin</Form.Label>
+          <Form.Check type='checkbox' lable='Is Admin' 
+          checked={isAdmin}
+          onChange={(e) => setIsAdmin(e.target.checked)}>
+    
+          </Form.Check>
+        </Form.Group>
+
+
+        <Button type='submit' variant='primary'>
+          Update
+        </Button>
+      </Form>
+  )}
+
+
+    
+    </FormContainer>
+
+    </>
+    
+  )
+}
+
+export default UserEditScreen
